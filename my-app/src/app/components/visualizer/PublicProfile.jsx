a4e36e57cc212d4e086003325ec42769bb1dc5b3
Larry Le <lle114@student.hbuhsd.edu>
2024-08-30 02:06:31 -0700

    followers and working on posts
    the public profile now accurately displays the number of followers and following. currently working on displaying user posts. if the user has no posts in their account, i will also work on dealing with that as well

--- my-app/src/app/components/visualizer/PublicProfile.jsx
+++ my-app/src/app/components/visualizer/PublicProfile.jsx
@@ -62,6 +62,87 @@ export default function PublicProfile({ images, objects }) {
     }
   }, [objects]);
 
+  const followersList = useMemo(() => {
+    let result;
+
+    if (objects.length !== 0) {
+      objects.forEach((object) => {
+        if (Array.isArray(object)) {
+          // if the array index at 0 has the instagram hyperlink then we know it's the followers list
+          // note that in this particular case, object is an array
+          if (object[0]["string_list_data"]) {
+            if (
+              object[0]["string_list_data"][0]["href"].includes("instagram.com")
+            ) {
+              console.log("followers list", object);
+              result = object;
+            }
+          }
+        }
+      });
+    }
+
+    return result;
+  }, [objects]);
+
+  const followingList = useMemo(() => {
+    let result;
+
+    if (objects.length !== 0) {
+      objects.forEach((object) => {
+        if (object["relationships_following"]) {
+          result = object["relationships_following"];
+        }
+      });
+    }
+
+    return result;
+  }, [objects]);
+
+  const postsList = useMemo(() => {
+    let result;
+
+    if (objects.length !== 0) {
+      objects.forEach((object) => {
+        if (Array.isArray(object)) {
+          // if one of the objects has a media key, then we know it's the user's posts, because all instagram posts have media (picture or video)
+          if (object[0]["media"]) {
+            result = object;
+          }
+        }
+      });
+    }
+
+    console.log(result);
+    return result;
+  }, [objects]);
+
+  const postImages = useMemo(() => {
+    let postURIs = [];
+    let result = [];
+
+    if (postsList) {
+      postsList.forEach((post) => {
+        if (post["media"]) {
+          // taking the first image media of a post (although i'm aware instagram posts can have more than one image or video)
+          for (let i = 0; i < post["media"].length; i++) {
+            if (
+              post["media"][i]["uri"].includes(".jpg") ||
+              post["media"][i]["uri"].includes(".png")
+            ) {
+              postURIs.push(post["media"][i]["uri"]);
+              break;
+            }
+          }
+        }
+      });
+
+      console.log(postURIs);
+    }
+
+    return postURIs;
+  }, [postsList]);
+
   useEffect(() => {
     console.log(objects);
   }, [objects]);
@@ -70,19 +151,27 @@ export default function PublicProfile({ images, objects }) {
     <>
       <section className="bg-prim-5 shadow-rough min-h-182 flex w-full flex-col gap-16 rounded-xl px-10 py-8">
         <div className="flex gap-16">
-          <div className="bg-prim-9 w-116 flex h-56 items-center justify-center rounded-xl transition duration-200 ease-in-out hover:scale-105">
+          <div className="bg-prim-9 flex h-56 w-80 flex-none items-center justify-center rounded-lg transition duration-200 ease-in-out hover:scale-105">
+            {!profilePicture && (
+              <>
+                <p className="font-xl animate-pulse font-league text-white">
+                  Loading
+                </p>
+              </>
+            )}
+
             {profilePicture && (
               <Image
                 src={profilePicture}
                 alt="User Image"
                 width={500}
                 height={500}
-                className="h-full w-full"
+                className="h-full w-full rounded-lg"
               />
             )}
           </div>
 
-          <div className="flex min-h-56 flex-col">
+          <div className="flex min-h-56 w-full flex-col">
             <article className="flex flex-col gap-3">
               <div className="flex flex-col">
                 <p className="text-prim-2 font-league text-4xl font-bold">
@@ -90,10 +179,10 @@ export default function PublicProfile({ images, objects }) {
                 </p>
                 <div className="flex gap-5">
                   <p className="text-prim-2 font-league text-lg">
-                    Followers: 216
+                    Followers: {followersList && followersList.length}
                   </p>
                   <p className="text-prim-2 font-league text-lg">
-                    Following: 234
+                    Following: {followingList && followingList.length}
                   </p>
                 </div>
               </div>
@@ -103,8 +192,16 @@ export default function PublicProfile({ images, objects }) {
 
             <div className="flex h-full flex-col justify-end">
               <p className="text-prim-2 font-league text-xl">
-                Followed by <span className="font-bold">Johnson</span> and{" "}
-                <span className="font-bold">Dean</span>
+                Followed by{" "}
+                <span className="font-bold">
+                  {followersList &&
+                    followersList[0]["string_list_data"][0]["value"]}
+                </span>{" "}
+                and{" "}
+                <span className="font-bold">
+                  {followersList &&
+                    followersList[1]["string_list_data"][0]["value"]}
+                </span>
               </p>
             </div>
           </div>
@@ -112,12 +209,36 @@ export default function PublicProfile({ images, objects }) {
 
         {/* you are able to add flex-grow or flex-shrink to children of a flexbox. setting it to 1/4 rounded each div to almost 1/3, the equivalent of setting width to 1/3 and accounting for the gap-3 */}
         <div className="flex flex-wrap gap-3">
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
-          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm" />
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[0]["title"]}
+            </p>
+          </div>
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[1]["title"]}
+            </p>
+          </div>
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[2]["title"]}
+            </p>
+          </div>
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[3]["title"]}
+            </p>
+          </div>
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[4]["title"]}
+            </p>
+          </div>
+          <div className="bg-prim-6 h-32 w-1/4 flex-grow rounded-sm">
+            <p className="text-prim-2 font-league text-xl">
+              {postsList && postsList[5]["title"]}
+            </p>
+          </div>
         </div>
       </section>
     </>
